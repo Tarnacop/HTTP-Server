63bf36ff58a0f5c60fb08073c1c3f07d1ab9dd8c
aig530 <aig530@student.bham.ac.uk>
2017-10-17 17:32:32 +0100

    Exercise 2: multi-threading added

--- accept_listen_socket.c
+++ accept_listen_socket.c
@@ -1,61 +1,122 @@
 /**
  * Created by Alexandru Blinda.
  */
-#include<netinet/in.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <memory.h>
+#include <pthread.h>
 
 #include "accept_listen_socket.h"
 #include "create_printable_address.h"
 #include "handle_client_socket.h"
 
-int accept_listen_socket(const int tcp6_socket) {
+/**
+ * Thread_control_block struct will hold the data that is thread related
+ */
+typedef struct thread_control_block {
 
     /**
-     * Declaring variables
-     * client holds the file descriptor of the accepted socket or -1 in case of error
-     * client_address holds the client address
-     * client_address_size holds the size of the client address
-     * buffer is passed to another method to create a printable address
-     * the extra 32 bytes to the buffer are needed for extra words
-     * printable will hold the actual printable string
-     */
+    * Declaring variables
+    * client holds the file descriptor of the accepted socket or -1 in case of error
+    * client_address holds the client address
+    * client_address_size holds the size of the client address
+    */
     int client;
-
     struct sockaddr_in6 client_address;
-    memset(&client_address, '\0', sizeof(client_address));
+    socklen_t client_address_size;
 
-    socklen_t client_address_size = sizeof(client_address);
+} thread_control_block_t;
 
+static void *client_thread(void *data) {
+
+    thread_control_block_t *tcb_pointer = (thread_control_block_t *) data;
+
+    /**
+    * buffer is passed to another method to create a printable address
+    * the extra 32 bytes to the buffer are needed for extra words
+    * printable will hold the actual printable string
+    */
     char buffer[INET6_ADDRSTRLEN + 64];
     char *printable;
 
     /**
-     * As long as we do not have an error, accept connections
+     * Create printable address from the client address
      */
-    while(client = accept(tcp6_socket,(struct sockaddr *) &client_address, &client_address_size) >= 0) {
+    printable = create_printable_address( &(tcb_pointer->client_address), buffer, sizeof(buffer));
 
-        /**
-         * Create printable address from the client address
-         */
-        printable = create_printable_address(&client_address, buffer, sizeof(buffer));
+    (void) handle_client_socket(tcb_pointer->client, printable);
 
-        (void) handle_client_socket(client, printable);
+    free(printable);
 
-        free(printable);
-    }
+    free(data);
+
+    pthread_exit(0);
+}
+
+
+int accept_listen_socket(const int tcp6_socket) {
 
     /**
-     * If we have an error
+     * As long as we do not have an error, accept connections
      */
-    if (client < 0) {
+    while(1) {
 
-        perror("Accepting client error occurred");
-        return -1;
+        /**
+         * Allocate space for a thread control block
+         * Because it is malloc'd, it is thread safe
+         */
+        thread_control_block_t *tcb_pointer = malloc (sizeof (thread_control_block_t));
+
+        if(tcb_pointer == NULL) {
+
+            perror("Could not allocate memory");
+            exit(1);
+        }
+
+        tcb_pointer->client_address_size = sizeof (tcb_pointer->client_address);
+
+        tcb_pointer->client = accept(tcp6_socket,
+                                     (struct sockaddr *) &(tcb_pointer->client_address),
+                                     &(tcb_pointer->client_address_size));
+
+        /**
+         * If we have an error
+         */
+        if (tcb_pointer->client < 0) {
+
+            perror("Accepting client error occurred");
+            free(tcb_pointer);
+            return -1;
+        }
+        else {
+
+            pthread_t thread;
+            pthread_attr_t pthread_attr; /** attributes for newly create thread */
+
+            if (pthread_attr_init(&pthread_attr) !=0) {
+
+                perror("Error creating initial thread attributes");
+                free(tcb_pointer);
+                return -1;
+            }
+
+            if(pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
+
+                perror("Setting thread attributes failed");
+                free(tcb_pointer);
+                return -1;
+            }
+
+            if(pthread_create(&thread, &pthread_attr, &client_thread, (void *)tcb_pointer)!= 0){
+
+                perror("Thread creation failed");
+                free(tcb_pointer);
+                return -1;
+            }
+        }
+
     }
 
-    return 0;
 } // END OF accept_listen_socket FUNCTION
--- handle_client_socket.c
+++ handle_client_socket.c
@@ -1,5 +1,5 @@
 /**
- * Created by Andreea Gheorghe on 16/10/2017.
+ * Created by Alexandru Blinda.
  */
 #include <stdio.h>
 #include <unistd.h>
@@ -9,13 +9,14 @@
 
 #define BUFFER_SIZE 1024
 
-int handle_client_socket(const int tcp6_socket, const char *const client_identification){
+int handle_client_socket(const int client_socket, const char *const client_identification){
 
     /**
      * Declaring variables
      * the buffer is used
      */
     char buffer[BUFFER_SIZE];
+    memset(buffer, '\0', BUFFER_SIZE);
     size_t bytes;
 
     printf("%s connected.\n", client_identification);
@@ -25,44 +26,31 @@ int handle_client_socket(const int tcp6_socket, const char *const client_identif
      * If there is an error, bytes will be negative and the loop will exit
      * We keep 1 space free to add a terminating null byte \0
      */
-    while( (bytes = read(tcp6_socket, buffer, BUFFER_SIZE - 1)) > 0) {
 
-        if (write(tcp6_socket, buffer, bytes) != bytes) {
+    bytes = read(client_socket, buffer, BUFFER_SIZE - 1);
+    if(bytes < 0) {
 
-            perror("Error writing");
-            return -1;
-        }
-
-        buffer[bytes] = '\0';
-
-        if (bytes >= 1 && buffer[bytes - 1] == '\n') {
-
-            if (bytes >= 2 && buffer[bytes - 2] == '\r') {
-
-                strcpy(buffer + bytes - 2, "..");
-            } else {
-
-                strcpy(buffer + bytes - 1, ".");
-            }
-        }
-
-#if (__SIZE_WIDTH__ == 64 || __SIZEOF_POINTER__ == 8)
-        printf("echoed %ld bytes back to %s, \"%s\"\n", bytes, client_identification, buffer);
-#else
-        printf ("echoed %d bytes back to %s, \"%s\"\n", bytes, tag, buffer);
-#endif
-    }
-
-    /* bytes == 0: orderly close; bytes < 0: something went wrong */
-    if (bytes != 0) {
-
-        perror("read");
+        perror("Error reading from socket");
+        close(client_socket);
         return -1;
     }
 
+    buffer[bytes] = '\0';
+    printf("Here is the message read: %s\n", buffer);
+
+    bytes = write(client_socket, buffer, bytes);
+
+    if(bytes < 0) {
+
+        perror("Error writing to socket");
+        close(client_socket);
+        return -1;
+    }
+
+    close(client_socket);
+
     printf("%s disconnected.\n", client_identification);
 
-    close(tcp6_socket);
 
     return 0;
 }
